cbbcd72b879510610a68f29e6e51b3aee84498f1
figueroa <jorgeluis.figueroabarroso@sysdata.it>
2023-04-18 19:01:29 +0200

    logging

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ venv\Scripts\activate
 pip install requests
 pip freeze > requirements.txt
 
-
+## provando un paquete
 python -m codigofacilito
--- codigofacilito/__main__.py
+++ codigofacilito/__main__.py
@@ -1,9 +1,21 @@
 from codigofacilito import unreleased
+import logging
+
+"""
+INFO -> 10
+DEBUG -> 20
+WARNING -> 30
+ERROR -> 40
+CRITICAL -> 50
+"""
+
+logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    print(">>> start paquete")
+    logging.debug(">>> start paquete")
     workshops = unreleased()
+    logging.debug("workshops:", workshops)
     print("workshops:", workshops)
 
-    print(">>> end paquete")
+    logging.debug(">>> end paquete")
 
